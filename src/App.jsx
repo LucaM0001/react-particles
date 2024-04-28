90535e5898511aeb99adb9f3b912b74c447f579e
Luca Mamitiana RANDRIANIAINA <lucamamitiana@gmail.com>
2024-04-28 08:29:48 +0300

    Creation du composant Particles et Affichage du texte

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import "./App.css";
+
+import ParticlesComponent from "./components/Particles";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="App">
+      <ParticlesComponent id="particles" />
+      <h1>Les particules sont tellement amusantes!</h1>
+    </div>
+  );
 };
 
 export default App;
